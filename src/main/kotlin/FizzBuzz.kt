6dcaea00280b17832ab94ad51dd247c7664c00c0
Bsitso <lohsitsofe60@gmail.com>
2021-10-26 09:30:09 +0000

    Refactor of FizzBuzz class

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -1,9 +1,9 @@
 class FizzBuzz {
     fun fizzBuzz(number: Int): String {
         return when {
-            number % 3 == 0 && number % 5 == 0 -> "FizzBuzz"
-            number % 3 == 0 -> "Fizz"
-            number % 5 == 0 -> "Buzz"
+            number.rem(3) == 0 && number.rem(5) == 0 -> "FizzBuzz"
+            number.rem(3) == 0 -> "Fizz"
+            number.rem(5) == 0 -> "Buzz"
             else -> "Not Fizz Buzz"
         }
     }
